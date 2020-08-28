fefcbf060fd99d5a03b5df62c014c634aecf924b
Mohamad Syazwan Ahmad Zubir <syazwan@invokeisdata.com>
2020-08-28 14:48:47 +0800

    Updates

---
+++ precache-manifest.3beecca280034a2b12721c426ae1cce6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9504f1fb19a2def688f8089f3a2b21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3396848d9487e8ef1fb3",
+    "url": "/static/css/2.4df217b3.chunk.css"
+  },
+  {
+    "revision": "2f7baf342e4000f87461",
+    "url": "/static/css/main.1b9f3207.chunk.css"
+  },
+  {
+    "revision": "3396848d9487e8ef1fb3",
+    "url": "/static/js/2.61278c71.chunk.js"
+  },
+  {
+    "revision": "2f7baf342e4000f87461",
+    "url": "/static/js/main.2fea81a4.chunk.js"
+  },
+  {
+    "revision": "b740e0a598976cd5e8cc",
+    "url": "/static/js/runtime-main.81edbfc1.js"
+  },
+  {
+    "revision": "4fd94c63db5327e5c0b336c1c958e269",
+    "url": "/static/media/lazada_banner.4fd94c63.png"
+  },
+  {
+    "revision": "9297a28ea405959b1f0c145087e4af8e",
+    "url": "/static/media/lazada_square.9297a28e.png"
+  },
+  {
+    "revision": "6bfd1f8ef0eea2c5dcc43115d8607687",
+    "url": "/static/media/shopee_banner.6bfd1f8e.png"
+  },
+  {
+    "revision": "f57b6a07d7e7e7a97d80381d182ec6ef",
+    "url": "/static/media/shopee_square.f57b6a07.png"
+  }
+]);
